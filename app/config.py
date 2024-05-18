8a34817d02661ce5a70f344c8710dbf4f4c201df
jonathannoel <jonathan.noel1@gmail.com>
2024-05-18 17:55:47 +0200

    Fixed files tracking

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    COUCHDB_SERVER = os.getenv('COUCHDB_SERVER', 'http://localhost:5984/')
-    COUCHDB_DATABASE = os.getenv('COUCHDB_DATABASE', 'insulin-database')
--- app/extensions.py
+++
@@ -1,3 +0,0 @@
-from flask_couchdb import CouchDB
-
-couch = CouchDB()
