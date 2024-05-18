891514c185169cac5f0c52b901a95a2f0080a657
jonathannoel <jonathan.noel1@gmail.com>
2024-05-18 17:56:02 +0200

    fixed tracked files

---
+++ README.md
@@ -0,0 +1,12 @@
+# HealthInfo
+HeathInfoProject
+
+# Requirements:
+Python with flask installed:
+pip install flask
+pip install pandas
+
+# Install dependencies
+- Python version 3.9
+- Install dependencies using python ```pip install -r requirements.txt```
+- Start docker containers (CouchDB & Déploiement flask (pour plus tard)) ```docker-compose up --build [-d]```
---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+
+from flask import Flask
+import couchdb
+
+
+def create_app():
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app.config.from_pyfile('../.env')
+    app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
+
+    # Connexion à CouchDB
+    couch = couchdb.Server(app.config['COUCHDB_SERVER'])
+    db = couch[app.config['COUCHDB_DATABASE']]
+    app.config['COUCHDB_DB'] = db
+
+    with app.app_context():
+        from .routes.insulin import insulin_blueprint
+        from .routes.meals import meals_blueprint
+        from .routes.index import home_blueprint
+
+        app.register_blueprint(insulin_blueprint, url_prefix='/api/v1/insulin')
+        app.register_blueprint(meals_blueprint, url_prefix='/api/v1/meals')
+        app.register_blueprint(home_blueprint)
+
+    return app
